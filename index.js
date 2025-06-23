f3045bed76b9b19dc03830849f3aea4074b5e685
kyzotom <Kyzo@post.sk>
2025-06-23 20:05:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const manifest = {
   id: "org.streamuj.mini",
   version: "1.0.0",
   name: "Mini Streamuj Addon",
-  description: "Testovaci addon pre nasadenie",
+  description: "Testovací addon pre nasadenie",
   types: ["movie"],
   idPrefixes: ["tt"],
   catalogs: [],
   resources: ["catalog"],
-  contactEmail: "kyzotom@gmail.com"
+  contactEmail: "tvoj@email.com"
 };
 
 const builder = new addonBuilder(manifest);
 
-// Dummy katalog handler (aj keď prázdny, musí byť definovaný)
 builder.defineCatalogHandler(() => {
   return Promise.resolve({ metas: [] });
 });
