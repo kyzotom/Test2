0c99a5201970fcdfd466123f33b7b799245c6bc0
kyzotom <Kyzo@post.sk>
2025-06-23 21:40:59 +0300

    Add files via upload

---
+++ index (10).js
@@ -0,0 +1,25 @@
+const { createServer } = require("http");
+const { addonBuilder } = require("stremio-addon-sdk");
+
+const manifest = {
+  id: "org.streamuj.mini",
+  version: "1.0.0",
+  name: "Mini Streamuj Addon",
+  description: "Testovací addon pre nasadenie",
+  types: ["movie"],
+  idPrefixes: ["tt"],
+  catalogs: [],
+  resources: ["catalog"],
+  contactEmail: "tvoj@email.com"
+};
+
+const builder = new addonBuilder(manifest);
+builder.defineCatalogHandler(() => {
+  return Promise.resolve({ metas: [] });
+});
+
+const interface = builder.getInterface();
+
+module.exports = (req, res) => {
+  interface(req, res);
+};
